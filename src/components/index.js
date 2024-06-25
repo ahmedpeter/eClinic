a2ee50f5433f0cd5b03b8324ff1ae68bfc508dad
Ahmed Peter <talk2ahmedpeter@gmail.com>
2024-06-25 14:24:16 +0100

    Added major components of quick clinic application

--- src/components/index.js
+++ src/components/index.js
@@ -7,6 +7,17 @@ import SignUp from "./auth/signup";
 import Sidebar from "./builder/Sidebar";
 import WelcomeScreen from "./auth/welcome";
 import Dashboard from "./dashboard";
+import ConsultantProfile from "./consultant/consultantProfile";
+import Chats from "./shared/chats";
+import MessageInbox from "./shared/inbox";
+import UserProfile from "./patient/profile";
+import History from "./patient/history";
+import Appointments from "./patient/appointments";
+import UpdateUserProfile from "./shared/updateProfile";
+// import MakeAppointment from "./patient/newAppointment";
+import MakeAppointment_1 from "./appointment/newAppointment_1";
+import MakeAppointment_2 from "./appointment/newAppointment_2";
+import MakeAppointment_3 from "./appointment/newAppointment_3";
 
 function Application() {
   return (
@@ -38,6 +49,34 @@ function Application() {
             <div className="main__content">
               <Routes>
                 <Route path="/app/dashboard" element={<Dashboard />} />
+                <Route
+                  path="/consultant/profile"
+                  element={<ConsultantProfile />}
+                />
+                <Route path="/messages/chat" element={<Chats />} />
+                <Route path="/messages" element={<MessageInbox />} />
+                <Route path="/profile/:id" element={<UserProfile />} />
+                <Route path="/patient/:id/history" element={<History />} />
+                <Route
+                  path="/patient/:id/new_appointment/step1"
+                  element={<MakeAppointment_1 />}
+                />
+                <Route
+                  path="/patient/:id/new_appointment/step2"
+                  element={<MakeAppointment_2 />}
+                />
+                <Route
+                  path="/patient/:id/new_appointment/step3"
+                  element={<MakeAppointment_3 />}
+                />
+                <Route
+                  path="/user/:id/update_profile"
+                  element={<UpdateUserProfile />}
+                />
+                <Route
+                  path="/appointment/con/:id/pat/:id/:id"
+                  element={<Appointments />}
+                />
               </Routes>
             </div>
           </div>
